11c0740eba89ed03e6e870a6a4193bd542ae3b42
gracepec <gracepec0621@gmail.com>
2025-04-01 09:39:22 +0900

    feat(baekjoon): [solved] ContinuousSum - DP

---
+++ baekjoon/ContinuousSum.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/1912
+
+n = int(input())
+P = list(map(int, input().split()))
+
+res = 0
+D = [0] * n
+D[0] = P[0]
+for i in range(1, n):
+    D[i] = max(D[i-1] + P[i], P[i])
+
+res = max(D)
+print(res)
