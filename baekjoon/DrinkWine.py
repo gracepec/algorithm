790f9cef5884309fbd401463e594f2b6582d1247
gracepec <gracepec0621@gmail.com>
2025-04-02 09:57:53 +0900

    feat(baekjoon): [solved] DrinkWine - DP

---
+++ baekjoon/DrinkWine.py
@@ -0,0 +1,11 @@
+# https://www.acmicpc.net/problem/2156
+
+n = int(input())
+P = [int(input()) for _ in range(n)]
+
+P.append(0)
+D = [0] * (n+2)
+for i in range(n):
+    D[i] = max(D[i-1], D[i-2]+P[i], D[i-3]+P[i-1]+P[i])
+    
+print(D[n-1])
