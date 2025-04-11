efa380e2c6c1be0ab2b3133452ee8aa0a00d1c9f
gracepec <gracepec0621@gmail.com>
2025-04-11 11:00:28 +0900

    faet(BOJ): [sol] AbsHeap.py - heap

---
+++ baekjoon/AbsHeap.py
@@ -0,0 +1,22 @@
+# https://www.acmicpc.net/problem/11286
+
+import sys
+import heapq
+
+input = sys.stdin.readline
+n = int(input())
+
+h = []
+for _ in range(n):
+    p = int(input())
+    if p != 0:
+        heapq.heappush(h, (abs(p),p))
+    else:
+        if h: 
+            print(heapq.heappop(h)[1])
+        else: 
+            print(0)
+
+'''
+# 절대값 힙
+'''
