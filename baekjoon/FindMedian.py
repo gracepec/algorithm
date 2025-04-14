1e6dfbd4ab8ed15c2ca80c41fce9f423c076be14
gracepec <gracepec0621@gmail.com>
2025-04-14 11:18:17 +0900

    fix: [sol] FindMedian - double heap

--- baekjoon/FindMedian.py
+++ baekjoon/FindMedian.py
@@ -1,10 +1,7 @@
-# https://www.acmicpc.net/problem/2696
-
-import sys
 import heapq
+import sys
 
 input = sys.stdin.readline
-inputs = sys.stdin.read
 
 t = int(input())
 for _ in range(t):
@@ -12,35 +9,29 @@ for _ in range(t):
     minh = []
     
     n = int(input())
-    if n <= 10: ###### 
-        P = list(map(int, input().split()))
-    else:
-        P = list(map(int, inputs().split())) # 라인수 명시 필요
+    P = []
+    for _ in range((n - 1) // 10 + 1):
+        P += list(map(int, input().split()))
 
-    od = n // 2 + 1
-    print(od)
-    mid = [0] * (od)
-    
-    heapq.heappush(maxh, -P[0])
-    mid[0] = P[0]
-    for i in range(1, n):
-        if not minh or P[i] > -maxh[0]:
-            heapq.heappush(minh, P[i])
-        elif P[i] <= -maxh[0]:
-            heapq.heappush(maxh, -P[i])
+    mid = []
+    for i, x in enumerate(P):
+        if not maxh or x <= -maxh[0]:
+            heapq.heappush(maxh, -x)
+        else:
+            heapq.heappush(minh, x)
 
-        if len(minh) >= len(maxh) + 2: 
-            heapq.heappush(maxh, -heapq.heappop(minh))
-        elif len(minh) < len(maxh):
+        if len(maxh) > len(minh) + 1:
             heapq.heappush(minh, -heapq.heappop(maxh))
-        
+        elif len(minh) > len(maxh):
+            heapq.heappush(maxh, -heapq.heappop(minh))
+
         if i % 2 == 0:
-            mid[i // 2] = minh[0]
-            
-    for i in range(0, n, 10):
+            mid.append(-maxh[0])
+
+    print(len(mid))
+    for i in range(0, len(mid), 10):
         print(*mid[i:i+10])
 
-'''
+''' 2696
 # 중앙값 구하기 - 이중힙
-""" 런타임에러 """
 '''
