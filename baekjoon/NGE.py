747697360813606f773727100dc349882abf62d6
gracepec <gracepec0621@gmail.com>
2025-04-21 12:28:40 +0900

    Create NGE.py

---
+++ baekjoon/NGE.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+P = list(map(int, input().split()))
+
+P.append(0)
+S = []
+for i in range(n):
+    if P[i] > P[i-1]:
+        S.append((i, P[i]))
+print(S)
+
+res = [0] * n
+pb = False
+for i in range(n):
+    for s in S:
+        if s[0] <= i:
+            pb = True
+        if s[1] > P[i]:
+            res[i] = s[1]
+        print(i, s, res[i], S)
+    if pb and S:
+        S.pop(0)
+    if res[i] == 0:
+         res[i] = -1
+
+print(res)
+
+'''17298
+# 오큰수
+'''
