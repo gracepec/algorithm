3d41c93b54e0b2b635d5f77f2f99c2ce561f4b42
gracepec <gracepec0621@gmail.com>
2025-04-22 15:11:01 +0900

    feat(pgm): [sol] Kth Number - sort

---
+++ programmers/KthNumber.py
@@ -0,0 +1,17 @@
+def solution(A, C):
+    res = []
+    for c in C:
+        i = c[0]
+        j = c[1]
+        k = c[2]
+        
+        An = A[i-1:j]
+        An.sort()
+        
+        res.append(An[k-1])
+    
+    return res
+
+''' 42748
+# 배열을 자르고 정렬하여 k번째 수 구하기
+'''
