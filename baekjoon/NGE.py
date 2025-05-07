c4a3ad5de5a0e91a80612ae1ab1533d8e11844f1
gracepec <gracepec0621@gmail.com>
2025-05-07 10:08:06 +0900

    feat(boj): [sol] NGE.py - stack

--- baekjoon/NGE.py
+++ baekjoon/NGE.py
@@ -4,29 +4,16 @@ input = sys.stdin.readline
 n = int(input())
 P = list(map(int, input().split()))
 
-P.append(0)
 S = []
-for i in range(n):
-    if P[i] > P[i-1]:
-        S.append((i, P[i]))
-print(S)
-
-res = [0] * n
-pb = False
-for i in range(n):
-    for s in S:
-        if s[0] <= i:
-            pb = True
-        if s[1] > P[i]:
-            res[i] = s[1]
-        print(i, s, res[i], S)
-    if pb and S:
-        S.pop(0)
-    if res[i] == 0:
-         res[i] = -1
-
-print(res)
-
+Res = []
+for p in reversed(P):
+    while S and S[-1] <= p:
+        S.pop()
+    Res.append(S[-1] if S else -1)
+    S.append(p)
+    
+print(*reversed(Res))
+    
 '''17298
 # 오큰수(오른쪽에 있으면서 큰 수 중 가장 왼쪽(가까이)에 있는 수) - stack
 '''
