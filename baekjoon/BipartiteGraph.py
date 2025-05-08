1b3801ce46718a2a799e4ea8db57b4dc4676867d
gracepec <gracepec0621@gmail.com>
2025-05-08 09:24:51 +0900

    fix(boj): [sol] BipartiteGraph.py - graph, dfs

--- baekjoon/BipartiteGraph.py
+++ baekjoon/BipartiteGraph.py
@@ -1,4 +1,5 @@
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 tc = int(input())
@@ -11,18 +12,26 @@ for i in range(tc):
         G[b].append(a)
 
     res = "YES"
-    bi = True
-    V = [False] * (ver+1)
-    def dfs(cn):
-        global bi, res
+    V = [-1] * (ver+1)
+    def dfs(cn, pnt):
+        global res
+        V[cn] = pnt
         for nn in G[cn]:
-            if V[nn] and V[cn][1] == V[nn][1]:
+            if V[nn] == -1:
+                dfs(nn, 1-pnt)
+            elif V[cn] == V[nn]:
                 res = "NO"
-            if not V[nn]:
-                bi = False if bi else True
-                V[nn] = (True, 0 if bi else 1)
-                dfs(nn)
-                bi = False if bi else True
-            
-    dfs(1)
+                
+    for i in range(1,ver+1):
+        if V[i] == -1:
+            dfs(i, 0)
+    
     print(res)
+
+''' 1707
+# 이분 그래프가 맞는지 리턴 - graph, dfs
+
+이분그래프는 여러 컴포넌트가 가능 (모두 연결된 그래프가 아닌) - line:25~27
+sys.setrecursionlimit(10**6) : 파이썬 최대 재귀 깊이를 늘려주는
+    - 기본값은 약 1,000
+'''
